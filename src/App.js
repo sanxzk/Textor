70545540ce738a05b444492a26d0e3936a6c1081
sanjana <sanjnasharma1411@gmail.com>
2023-12-05 16:07:37 +0530

    created About and Textform

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
+import Textform from './components/Textform';
  
 function App() {
   return (
-   <>
-    <Navbar/>
-   </>
+    <>
+    <Navbar title = "Textor"/>
+    {/* <Navbar/> */}
+    {/* <Navbar title="Textor"/> */}
+
+    {/* <Textform/> */}
+    <div className="container my.3">
+      <Textform heading = "Enter the text"/>
+      <About/>
+    </div>
+   </> 
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,37 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Navbar() {
+
+export default function Navbar(props) {
+  const title = props.title; 
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Textor</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">{props.title}</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About</a>
         </li>   
       </ul>
-      <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button class="btn btn-outline-success" type="submit">Search</button>
+      <form className="d-flex" role="search">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button className="btn btn-outline-success" type="submit">Search</button>
     </form>
     </div>
   </div>
 </nav>
   )
 }
+
+Navbar.propTypes = {title: PropTypes.string.isRequired}
+
+Navbar.defaultProps ={
+  title: 'set title here'
+};
